2fbbeb2b623b625f822eddf2bfc03e3744ce4f44
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2016-03-10 19:14:22 +0200

    Generate project with Yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name simonGameApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the simonGameApp
+ */
+angular.module('simonGameApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name simonGameApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the simonGameApp
+ */
+angular.module('simonGameApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
