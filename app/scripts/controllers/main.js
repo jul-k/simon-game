50c53e26cc982f68f7316d319c28df4620777a95
Julia Kudryavtseva <juliet.kudryavtseva@gmail.com>
2016-04-05 21:51:58 +0300

    Play audio onclick; change opacity of clicked el

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,46 @@
  */
 var app = angular.module('simonGameApp');
 
-app.controller('MainCtrl', ['$scope', function ($scope) {
+app.controller('MainCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
     $scope.numberOfSteps = 0;
     $scope.startBtn = 'ON';
+
+    $scope.startFn = function () {
+        if ($scope.startBtn === 'ON') {
+            $scope.startBtn = 'OFF';
+        } else {
+            $scope.startBtn = 'ON';
+        }
+    };
+
+    $scope.changeColor = function(e) {
+        angular.element(e.target).css('opacity', '0.8');
+        $timeout(function() {
+            angular.element(e.target).css('opacity', '1');
+        }, 500);
+    };
+
+    $scope.audioGreen = function (e) {
+        var audio = document.getElementById("audio-green");
+        audio.play();
+        $scope.changeColor(e);
+    };
+
+    $scope.audioRed = function (e) {
+        var audio = document.getElementById("audio-red");
+        audio.play();
+        $scope.changeColor(e);
+    };
+
+    $scope.audioYellow = function (e) {
+        var audio = document.getElementById("audio-yellow");
+        audio.play();
+        $scope.changeColor(e);
+    };
+
+    $scope.audioBlue = function (e) {
+        var audio = document.getElementById("audio-blue");
+        audio.play();
+        $scope.changeColor(e);
+    };
 }]);
